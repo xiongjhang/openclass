6b2b0a9694c93982e028751ea95d39a4cf75bf82
cisia_ps <jhang.xiong@gmail.com>
2025-05-17 10:14:53 +0800

    init lab00

---
+++ cs231n/labs/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_five():
+    """Come up with the most creative expression that evaluates to 2025
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_five()
+    2025
+    """
+    return 1**3 + 2**3 + 3**3 + 4**3 + 5**3 + 6**3 + 7**3 + 8**3 + 9**3
+
