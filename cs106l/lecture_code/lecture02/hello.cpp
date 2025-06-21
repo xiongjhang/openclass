81a235f3812f8b8cc539cf642cd88e14a87dcd8b
xiong <jhang.xiong@gmail.com>
2025-06-21 12:47:44 +0800

    update cs106l lecture02

---
+++ cs106l/lecture_code/lecture02/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+#include <cmath>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to" << std::endl;
+    for (char ch : "CS106L") {
+        std::cout << ch << std::endl;
+    }
+}
